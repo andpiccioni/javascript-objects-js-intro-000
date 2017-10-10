044277b026eead0e35cdb864417614f52a585abb
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-10 21:14:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,8 +8,8 @@ updatePlaylist(playlist, "Celentano", "Azzurro")
 updatePlaylist(playlist, "Slowdive", "Beat it")
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.[artistName]
-  
+  delete playlist['artistName']
+
   return playlist
 }
 
