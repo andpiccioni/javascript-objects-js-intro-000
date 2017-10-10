bb3713deae20c253faa945a8ca427c402e6281c2
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-10 20:29:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { pink_floyd : "the wall" }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName]=songTitle"value"
+  playlist[artistName]=songTitle
   return playlist
 }
 updatePlaylist(playlist, "Celentano", "Azzurro")
